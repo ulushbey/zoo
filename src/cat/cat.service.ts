8a14750f9266731b23606e05ac073b504274354b
ulushbey <ulushbey.h@gmail.com>
2024-05-06 18:27:03 +0900

    Initial Setup

---
+++ src/cat/cat.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CatService {
+  public getHello(): string {
+    return 'MYEOW';
+  }
+  public introduce(): string {
+    return 'My name is TOM!';
+  }
+}
